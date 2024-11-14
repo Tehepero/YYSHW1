a224feb7fc54b1febbbe7471baef05dbf0d384bc
Blue <isyuyong6732@naver.com>
2024-11-15 00:18:04 +0900

    새로운 YYSHW1 프로젝트 생성 및 Github에 레포지토리 추가 후 연동 첫 커밋 및 푸쉬 테스트

---
+++ src/main/java/com/yys/yyshw1/Yyshw1Application.java
@@ -0,0 +1,17 @@
+package com.yys.yyshw1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 현재 실력으로 만들수 있는게 많이 없어서
+// 추천해주신 강의 5-9 실습을 통해 한번 더 작성해보았습니다..
+// 1. 깃연동 실패 및 재시도중
+
+@SpringBootApplication
+public class Yyshw1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yyshw1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/yys/yyshw1/Yyshw1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yys.yyshw1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yyshw1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
