d9c26ab2ddac6d8773149cff0b653d029f482cf4
Blue <isyuyong6732@naver.com>
2024-11-15 00:21:06 +0900

    새로운 YYSHW1 프로젝트 생성 및 Github에 레포지토리 추가 후 연동 첫 커밋 및 푸쉬 테스트

--- src/main/java/com/yys/yyshw1/Yyshw1Application.java
+++ src/main/java/com/yys/yyshw1/Yyshw1Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // 현재 실력으로 만들수 있는게 많이 없어서
 // 추천해주신 강의 5-9 실습을 통해 한번 더 작성해보았습니다..
 // 1. 깃연동 실패 및 재시도중
+// 커밋 및 푸쉬 성공 왜 main이랑 maste로 나뉘었는지 모르겠다
 
 @SpringBootApplication
 public class Yyshw1Application {
